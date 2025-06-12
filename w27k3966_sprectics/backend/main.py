129b3cfc941d58c1885905ab36f158154bff659e
soheeshins <w27k3966@gmail.com>
2025-06-12 12:05:03 +0900

    ff

--- w27k3966_sprectics/backend/main.py
+++ w27k3966_sprectics/backend/main.py
@@ -11,4 +11,9 @@ def hello_name(name):
     print(f'hello {name}')
     return { 'message': 'hello', 'name': name }
 
+@app.route('/user/<user_id>')
+def hello_name(user_id):
+    print(f'user {user_id}')
+    return { 'message': 'user_id', 'user_id': id }
+
 app.run(debug=True, host='0.0.0.0', port=5001)
