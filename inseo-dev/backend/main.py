f6848365a4e28c9484efcce690f2ec8944cd8b98
zzangis34 <43721201+zzangis34@users.noreply.github.com>
2025-06-16 13:57:42 +0900

    asd

--- inseo-dev/backend/main.py
+++ inseo-dev/backend/main.py
@@ -1,24 +1,115 @@
-from flask import Flask
+from flask import Flask, request
+import pymysql
+from datetime import datetime
+
+def get_connection():
+    return pymysql.connect(
+        host='database-1.cts2qeeg0ot5.ap-northeast-2.rds.amazonaws.com',
+        user='kevin',
+        password='spreatics*',
+        db='instagram_inseo',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 app = Flask(__name__)
-
-@app.route('/hello')
-def hello():
-    print('hello')
-    return { 'message': 'hello' }
-
-@app.route('/hello/<name>')
-def hello_name(name):
-    print(f'hello {name}')
-    return { 'message': 'hello', 'name': name }
-
-@app.route('/users')
+# 사용자 생성
+@app.route('/users', methods=['POST'])
 def users():
-    return {'message': 'users'}
+    data = request.get_json
+    nickname = data.get('nickname')
+    name = data.get('name')
+    password = data.get('password')
+    age = data.get('age')
+    email = data.get('email')
+    
+    if nickname is None or name is None or password is None:
+        return {"status": "failed", "reason": "nickname, name, password is None"}
 
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        try:
+            sql = """
+            INSERT INTO users (nickname, name, password, age, email) 
+                values (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (nickname, name, password, age, email))
+            conn.commit()
+
+        except pymysql.err.IntegrityError as e:
+            return { "status": "failed", "reason": str(e) }
+    
+    with conn.cursor() as cursor:
+        sql = """
+        SELECT user_id
+        FROM users
+        WHERE nickname = %s
+        """
+        cursor.execute(sql,(nickname,))
+        return {"status": "success", "user_id": cursor.fetchone()}
+
+# 특정 사용자 조회
 @app.route('/users/<user_id>')
-def users_id(user_id):
-    return {'message':'success', 'user_id': user_id}
+def search_user(user_id):
+    conn = get_connection()
 
+    with conn.cursor() as cursor:
+        sql = """
+        SELECT * 
+        FROM users
+        WHERE user_id = %s
+        """
+        cursor.execute(sql,(user_id,))
+        row = cursor.fetchone()
+
+        # 입력한 user_id 가 db에 없는 경우
+        if row is None:
+            return {"status": "failed", "reason": f"user_id, {user_id} doesn't exist."}
+
+        return {"status": "success", "user": row}
+
+# 사용자 정보 수정
+@app.route('/users/<user_id>', methods = ['PUT'])
+def update_user(user_id):
+    data = request.json
+    nickname = data.get('nickname')
+    name = data.get('name')
+    password = data.get('password')
+    age = data.get('age')
+    email = data.get('email')
+
+    conn = get_connection()
+
+    with conn.cursor() as cursor:
+        try:
+            sql_select = """
+            SELECT *
+            FROM users
+            WHERE user_id = %s
+            """
+            cursor.execute(sql_select,(user_id,))
+            row = cursor.fetchone()
+
+            # 입력한 user_id 가 db에 없는 경우
+            if row is None:
+                return {"status": "failed", "reason": f"user_id, {user_id} doesn't exist."}
+
+            sql_update = """
+            UPDATE users SET nickname = %s, name = %s, password = %s, age = %s, email = %s WHERE user_id = %s
+            """
+            cursor.execute(sql_update, (nickname, name, password, age, email,user_id))
+            conn.commit()
+            
+            row['nickname'] = nickname
+            row['name'] = name
+            row['password'] = password
+            row['age'] = age
+            row['email'] = email
+            
+            return {"status":"success", "user":row}
+
+        except pymysql.err.IntegrityError as e:
+            return { "status": "failed", "reason": str(e) }
 
 app.run(debug=True, host='0.0.0.0', port=5000)
