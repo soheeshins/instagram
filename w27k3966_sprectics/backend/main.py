3a94074b897d1b5756a66c64e69bacb2b3cc9249
soheeshins <w27k3966@gmail.com>
2025-06-16 11:48:32 +0900

    users

--- w27k3966_sprectics/backend/main.py
+++ w27k3966_sprectics/backend/main.py
@@ -1,24 +1,52 @@
+import pymysql
 from flask import Flask, request
 
 app = Flask(__name__)
+
+def get_connection():
+    return pymysql.connect(
+        host='database-1.cts2qeeg0ot5.ap-northeast-2.rds.amazonaws.com',
+        user='kevin',
+        password='spreatics*',
+        db='instagram_kevin',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 @app.route('/users', methods=['POST'])
 def create_user():
-    
     data = request.get_json()
-    print(data)
 
+    # 입력값 추출
     nickname = data['nickname']
     name = data['name']
     password = data['password']
     age = data.get('age')
     email = data.get('email')
 
-    user_id = 105
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        try:
+            sql = """
+            INSERT INTO users (nickname, name, password, age, email)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(sql, (nickname, name, password, age, email))
+            conn.commit()
+            user_id = cursor.lastrowid
+
+        except pymysql.err.IntegrityError as e:
+            conn.rollback()
+            return {
+                "status": "failed",
+                "reason": f"nickname, {nickname} is duplicated"
+            }, 409
+
+    conn.close()
 
     return {
-        'status': 'created',
-        'user_id': user_id,
-        'email': email
-    }
+        "status": "created",
+        "user_id": user_id
+    }, 201
 
 app.run(debug=True, host='0.0.0.0', port=5001)
